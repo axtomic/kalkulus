ff09f2e6a81387b7fbad0798c897a852852345aa
axtomic <alexgoreng26@gmail.com>
2024-10-09 08:37:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// var slide = 1;
+// {
+//   showSlide(slide);
+// }
+
+// function nextSlide(n) {
+//   showSlide((slide += n));
+// }
+
+// function showSlide(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("taskSlide");
+
+//   if (n > slides.length) {
+//     slide = 1;
+//   }
+
+//   if (n < 1) {
+//     slide = slides.length;
+//   }
+
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+
+//   slides[slide - 1].style.display = "block";
+// }
